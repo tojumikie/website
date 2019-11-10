68204a5717407f077100cfae8aef10abd82c3c8e
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2019-11-09 20:42:58 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+/**
+ *
+ * @author tojum
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This application will print out Hello World!
+        System.out.println("Hello World!");
+    }
+}
